42664ebd2dc56de7a7e678926ca9264b830e47e6
moaaz82abdo <moaazabdo31@gmail.com>
2024-10-09 20:19:59 +0300

    -a

--- src/components/FindATeacher.jsx
+++ src/components/FindATeacher.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import '../App.css'
 function FindATeacher() {
   return (
-    <div>FindATeacher</div>
+    <div className='select'>
+        <select name="select" id="">
+            Arabic
+            <option value="1">Arabic</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+        </select>
+        <select name="select" id="">
+            Arabic
+            <option value="1">Lesson category</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+        </select>
+        <select name="select" id="">
+            Arabic
+            <option value="1">level</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+        </select>
+    </div>
   )
 }
 
